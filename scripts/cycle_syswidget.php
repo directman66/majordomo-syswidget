aebe7136575ee4d114ed7728df8ed6af4d6e4abb
directman66 <sannikovdi@yandex.ru>
2018-05-31 16:18:05 +0500

    Update cycle_syswidget.php

--- scripts/cycle_syswidget.php
+++ scripts/cycle_syswidget.php
@@ -10,21 +10,25 @@ include_once("./load_settings.php");
 include_once(DIR_MODULES . "control_modules/control_modules.class.php");
 $ctl = new control_modules();
 include_once(DIR_MODULES . 'syswidget/syswidget.class.php');
-
 $syswidget_module = new syswidget();
 $syswidget_module->getConfig();
-
+// In data
+//$tmp1 = SQLSelectOne("SELECT ID FROM lgps_out LIMIT 1");
+// Out data
+//$tmp2 = SQLSelectOne("SELECT ID FROM lgps_in LIMIT 1");
+//if ((!$tmp1['ID']) && (!$tmp2['ID']))
+//   exit; // no devices added -- no need to run this cycle
  
 echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
 $latest_check=0;
-$checkEvery=60; // poll every 1 min
+$checkEvery=300; // poll every 5 min
 while (1)
 {
    setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
    if ((time()-$latest_check)>$checkEvery) {
     $latest_check=time();
     //echo date('Y-m-d H:i:s').' Polling devices...\n';
-    $syswidget->processCycle();
+    $syswidget_module->processCycle();
    }
    if (file_exists('./reboot') || IsSet($_GET['onetime'])){
       $db->Disconnect();
